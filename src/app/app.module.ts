5e5d9cea398cdebb40ab6c18954dd00501d1928f
Andrew1995bono <andrew.matseliukh@gmail.com>
2022-08-08 16:57:50 +0300

    Agm-map was integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {AgmCoreModule} from "@agm/core";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAjklH_g2KrowTJOr78fMFsdIokyUa4lgM'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
